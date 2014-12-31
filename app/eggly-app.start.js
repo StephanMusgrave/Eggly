ebf6f4c8e9451522bd32157286650794b144fd85
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-12-31 14:01:49 +0000

    Filters

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -18,5 +18,18 @@ angular.module('Eggly', [])
       {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humour" },
       {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humour" }
     ];
+
+    $scope.currentCategory = null;
+
+    function setCurrentCategory(category) {
+      $scope.currentCategory = category;
+    }
+
+    function isCurrentCategory(category) {
+      return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
+    }
+
+    $scope.setCurrentCategory = setCurrentCategory;
+    $scope.isCurrentCategory  = isCurrentCategory;
   })
 ;
