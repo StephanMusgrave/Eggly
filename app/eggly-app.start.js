d6425f1419109ebc1f589df3d10863cb81087386
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2015-01-01 14:54:56 +0000

    Simple States - set up editing and creating states

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -23,6 +23,8 @@ angular.module('Eggly', [])
 
     function setCurrentCategory(category) {
       $scope.currentCategory = category;
+      cancelCreating();
+      cancelEditing();
     }
 
     function isCurrentCategory(category) {
@@ -31,5 +33,44 @@ angular.module('Eggly', [])
 
     $scope.setCurrentCategory = setCurrentCategory;
     $scope.isCurrentCategory  = isCurrentCategory;
+
+    //-------------------------------------------------------------------------------------------------
+    // CREATING AND EDITING STATES
+    //-------------------------------------------------------------------------------------------------
+    function shouldShowCreating() {
+        return $scope.currentCategory && !$scope.isEditing;
+    }
+
+    function startCreating() {
+        $scope.isCreating = true;
+        $scope.isEditing = false;
+    }
+
+    function cancelCreating() {
+        $scope.isCreating = false;
+    }
+
+    $scope.shouldShowCreating = shouldShowCreating;
+    $scope.startCreating = startCreating;
+    $scope.cancelCreating = cancelCreating;
+
+    function shouldShowEditing() {
+        return $scope.isEditing && !$scope.isCreating;
+    }
+
+    function startEditing() {
+        $scope.isCreating = false;
+        $scope.isEditing = true;
+    }
+
+    function cancelEditing() {
+        $scope.isEditing = false;
+        $scope.editedBookmark = null;
+    }
+
+    $scope.startEditing = startEditing;
+    $scope.cancelEditing = cancelEditing;
+    $scope.shouldShowEditing = shouldShowEditing;
+
   })
 ;
