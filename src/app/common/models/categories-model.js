6098937f7ba0119e7153b3ae7b76003092c66f5e
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2015-01-06 18:45:08 +0000

    Control promises with

--- src/app/common/models/categories-model.js
+++ src/app/common/models/categories-model.js
@@ -21,8 +21,24 @@ angular.module('eggly.models.categories', [
       return (categories) ? $q.when(categories) : $http.get(URLS.FETCH).then(cacheCategories);
     };
 
-    model.getCategoryByName = function() {
+    model.getCategoryByName = function(categoryName) {
       var deferred =$q.defer();
+      
+      function _.findCategory() {
+        return _.find(categories,function(c){
+          return c.name == categoryName;
+        })
+      }
+
+      if(categories) {
+        deferred.resolve(findCategory());
+      } else {
+        model,getCategories()
+          .then(function(result){
+            deferred.resolve(findCategory());
+          })
+      }
+      
       return deferred.promise;
     };
   })
